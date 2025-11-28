7d10646ae7bce41acd83f0451b041b76ae389ea4
rexek60 <rexeko601@gmail.com>
2025-11-28 11:25:10 +0300

    Delete ShopItem.js

--- ShopItem.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ShopSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  cost: Number
-});
-
-export default mongoose.model("ShopItem", ShopSchema);
