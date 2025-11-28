bb39849b3260603e37dbf09b5259b1e0fa070071
rexek60 <rexeko601@gmail.com>
2025-11-28 11:24:55 +0300

    Delete Score.js

--- Score.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ScoreSchema = new mongoose.Schema({
-  wallet: String,
-  score: Number,
-  date: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Score", ScoreSchema);
